95d00b9f7208d75ac9539e1651bfca1e055c0f0e
Henry Chen <chenry@Henrys-Laptop.lan>
2024-06-03 13:21:40 +0800

    released for 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "timmytimesheet",
-        version = "0.1",
+        version = "0.1.1",
         description = "a basic timesheeting cli",
         author="jumper385 (Henry Chen)",
         install_package_data=True,
