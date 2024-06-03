e2b9bfcdd72ac34669a969ee77f9e6649e945514
Henry Chen <chenry@Henrys-Laptop.lan>
2024-06-03 13:12:17 +0800

    updated requriements

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = "timmy",
+        name = "timmy-time",
         version = "0.1",
         description = "a basic timesheeting cli",
         author="jumper385 (Henry Chen)",
         install_package_data=True,
         install_requires = [
-            "typer[all]",
-            ],
+            "typer[all]"],
         packages = find_packages(),
         keywords = ["timesheeting"],
         entry_points = {
             'console_scripts': [
-                'timmy=timmy.main:app',
-                ]},
-            )
+                'timmy-time=timmy.main:app']},
+        classifiers = [
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            ])
 
